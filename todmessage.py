7c9e6d327aa777ffbe6b9b961c9d3d2d58f7be86
GooseterV <goosevincent@gmail.com>
2021-06-06 16:36:51 -0400

    a

--- todmessage.py
+++ todmessage.py
@@ -3,13 +3,11 @@ import threading
 from threading import Thread
 
 
-
-def add_message():
-    name = input("What is your Blooket name?")
-    msge = input("What message would you like to send?")
-    token = input("What is your Blooket account token? (JWT blahblah)")
+def add_tokens():
+    amount = input("How many tokens?")
+    token = input("What is your Blooket account token? (JWT blahblah)")   
     msg = f"{name}: {msge}"
-    request = requests.put("https://api.blooket.com/api/towers/message", data={"message":msg}, headers={"authorization":token})
+    request = requests.put("https://api.blooket.com/api/users/addtokens", data={"addedTokens":amount}, headers={"authorization":token, "referer": "https://www.blooket.com"})
     print(request.text)
 
-add_message()
+add_tokens()
