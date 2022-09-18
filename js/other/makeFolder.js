00486d02640e7abab41b97410416cf71b4b3ca61
GooseterV <dangoenjoyer@gmail.com>
2022-09-18 13:02:51 -0400

    add a couple more scripts

--- js/other/makeFolder.js
+++ js/other/makeFolder.js
@@ -2,5 +2,5 @@
 	function reactHandler() {
 		return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
 	};
-	reactHandler().stateNode.setState({ plus: true });
+	reactHandler().stateNode.setState({ creatingFolder: true });
 })();
