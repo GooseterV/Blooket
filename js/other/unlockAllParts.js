5a03055b0f89726debe149c3365252cf5ccfecee
GooseterV <dangoenjoyer@gmail.com>
2022-09-28 21:22:14 -0400

    asgasggaws

---
+++ js/other/unlockAllParts.js
@@ -0,0 +1,9 @@
+
+(() => {
+    
+	const state = Object.values(document.querySelector("#app > div > div > div:nth-child(9)"))[1].children[1]._owner.stateNode;
+    Object.keys(state.state.unlocks).forEach((u)=>{
+        state.state.unlocks[u] = (new Array(101)).fill(null).map((x, i)=>i);
+    });
+    state.setState({base:0});
+})();
