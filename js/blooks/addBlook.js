c025c7bb569505ba4e771e0b9d0cb3da760081f7
Greenlio <62725108+Greenlio@users.noreply.github.com>
2022-10-04 23:12:14 -0500

    better practice
    coauthor @Minesraft2 for help

--- js/blooks/addBlook.js
+++ js/blooks/addBlook.js
@@ -4,7 +4,11 @@ const iF = document.createElement("iframe");
 iF.src = "about:blank";
 iF.style = "display:none;"
 document.body.appendChild(iF);
-const blookName = iF.contentWindow.prompt("What's the name of the blook? (must exist)");
-const blookAmount =  iF.contentWindow.prompt("How many do you want?");
-Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.state.blookData[blookName] = parseInt(blookAmount)
-Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.forceUpdate()
+Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.setState(
+  state => (
+    {...state, blookData: (
+      state.blookData[iF.contentWindow.prompt("What's the name of the blook? (must exist)")] = parseInt(iF.contentWindow.prompt("How many do you want?")), state.blookData
+    )
+    }
+  )
+)
