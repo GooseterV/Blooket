c2831151ac6016a0465ae9affb6eb47173af2625
Greenlio <62725108+Greenlio@users.noreply.github.com>
2022-10-04 23:12:48 -0500

    lol

--- js/blooks/addBlook.js
+++ js/blooks/addBlook.js
@@ -8,7 +8,6 @@ Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.
   state => (
     {...state, blookData: (
       state.blookData[iF.contentWindow.prompt("What's the name of the blook? (must exist)")] = parseInt(iF.contentWindow.prompt("How many do you want?")), state.blookData
-    )
-    }
+    )}
   )
 )
