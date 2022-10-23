f4fc3fea872dc22ed87831874db964963f6e73cc
Gus <82774618+GooseterV@users.noreply.github.com>
2022-10-23 18:50:22 -0400

    Update unlockAllParts.js

--- js/other/unlockAllParts.js
+++ js/other/unlockAllParts.js
@@ -1,9 +1,7 @@
-
 (() => {
-    
 	const state = Object.values(document.querySelector("#app > div > div > div:nth-child(9)"))[1].children[1]._owner.stateNode;
-    Object.keys(state.state.unlocks).forEach((u)=>{
-        state.state.unlocks[u] = (new Array(101)).fill(null).map((x, i)=>i);
-    });
-    state.setState({base:0});
+    	Object.keys(state.state.unlocks).forEach((u)=>{
+        	state.state.unlocks[u] = (new Array(101)).fill(null).map((x, i)=>i);
+    	});
+    	state.setState({base:0});
 })();
