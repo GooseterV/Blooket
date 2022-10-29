93c3fe7f6eed23bd832c4afb45a984bd0eab7b4c
Greenlio <62725108+Greenlio@users.noreply.github.com>
2022-10-29 18:21:42 -0500

    Cc previous commit

--- js/other/freePlus.js
+++ js/other/freePlus.js
@@ -1,6 +1,4 @@
 (() => {
-	function reactHandler() {
-		return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
-	};
-	reactHandler().stateNode.setState({ plus: true });
+	const stateNode = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode;
+	stateNode.setState({ plus: true });
 })();
