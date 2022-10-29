d5ea19868f83cdcec6f76de099ae06961112faf9
Greenlio <62725108+Greenlio@users.noreply.github.com>
2022-10-29 18:17:56 -0500

    Better practice / looks nicer

--- js/blooks/spoofer.js
+++ js/blooks/spoofer.js
@@ -1,8 +1,6 @@
-/* Spoofer.js */
+/* spoofer.js */
 const ALL_BLOOKS = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find(x=>x.exports?.a?.toString().includes('"Rainbow Astronaut":')).exports.a.toString().split('"').filter((x, i) => i % 2 !== 0).filter(x=>!["Common", "Uncommon", "Rare", "Epic", "Legendary", "Chroma", "Mystical"].includes(x));
 const allBlooks = ALL_BLOOKS.reduce((a,b)=> (a[b]=1,a),{});
+const stateNode = Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode
 
-
-document.getElementById("app").firstElementChild.firstElementChild.lastElementChild.children[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.click();
-
-Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.setState({ blookData: allBlooks });
+stateNode.setState({ blookData: allBlooks });
